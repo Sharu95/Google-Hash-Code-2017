fb8bf42b8579b687bdcf0a4a6bbc5df6b3aaac9e
Perelan <sinriat@gmail.com>
2017-02-23 20:36:28 +0100

    Added a node

--- src/Cache.java
+++ src/Cache.java
@@ -1,9 +1,35 @@
 public class Cache {
+
+    private class Video {
+        int freq, size;
+        Video next;
+        Video (int freq, int size) {
+            this.freq = freq;
+            this.size = size;
+        }
+    }
+
     private int id, capacity;
+    private Video head;
+    private final int TRESHHOLD = capacity / 2;
 
     public Cache (int id, int capacity) {
         this.id = id;
         this.capacity = capacity;
+    }
+
+    public void addVideo (int freq, int size) {
+
+
+        if ((size + capacity) > capacity) {
+            System.out.println();
+            return;
+        }
+
+        if (head == null) {
+
+            // head = new Video();
+        }
     }
 
     public int getId () {
